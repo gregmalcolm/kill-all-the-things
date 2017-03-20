ba9f27befae1169c99e8df0ceb93ecf8c88d3dbf
Greg Malcolm <gmalcolm@icct.com>
2017-03-20 19:57:45 -0400

    We completely converted the 2d space demo to es6

--- src/objects/Player/Player.js
+++ src/objects/Player/Player.js
@@ -5,12 +5,66 @@ export default class Player extends Phaser.Sprite {
 
         game.physics.arcade.enable(this);
         game.add.existing(this);
+
+        this.body.drag.set(70);
+        this.body.maxVelocity.set(200);
+
+        this.weapon = this._addWeapon(game);
     }
 
-    updatePlayer(cursors, contacts) {
-        //this._handleInput(cursors, contacts);
+    respond(cursors, fireButton) {
+        this._handleInput(cursors, fireButton);
+
+        this.game.world.wrap(this, 16);
     }
 
-    _handleInput(cursors, contacts) {
+    render() {
+        this.weapon.debug();
     }
+
+    _handleInput(cursors, fireButton) {
+        if (cursors.up.isDown) {
+            this.game.physics.arcade.accelerationFromRotation(
+                this.rotation, 300, this.body.acceleration);
+        } else {
+            this.body.acceleration.set(0);
+        }
+
+        if (cursors.left.isDown) {
+            this.body.angularVelocity = -300;
+        } else if (cursors.right.isDown) {
+            this.body.angularVelocity = 300;
+        } else {
+            this.body.angularVelocity = 0;
+        }
+
+        if (fireButton.isDown) {
+            this.weapon.fire();
+        }
+    }
+
+
+    _addWeapon(game) {
+        //  Creates 30 bullets, using the 'bullet' graphic
+        let weapon = game.add.weapon(30, 'bullet');
+
+        //  The bullet will be automatically killed when it leaves the world bounds
+        weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+
+        //  The speed at which the bullet is fired
+        weapon.bulletSpeed = 600;
+
+        //  Speed-up the rate of fire, allowing them to shoot 1
+        //  bullet every 60ms
+        weapon.fireRate = 100;
+
+        //  Tell the Weapon to track the 'player' Sprite
+        //  With no offsets from the position
+        //  But the 'true' argument tells the weapon to track
+        //  sprite rotation
+        weapon.trackSprite(this, 0, 0, true);
+
+        return weapon;
+    }
+
 }
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -10,5 +10,12 @@ export default class GameState extends Phaser.State {
 
     create() {
         this.player = new Player(this.game, 400, 300);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    }
+
+    update() {
+        this.player.respond(this.cursors, this.fireButton);
     }
 }
